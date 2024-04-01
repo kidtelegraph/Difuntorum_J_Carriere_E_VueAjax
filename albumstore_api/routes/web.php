fe4c16c9869e0561d9e16de8764b60ae8d8c5e87
kidtelegraph <jxen.dee@gmail.com>
2024-04-01 05:24:35 -0400

    lumen_api
    lumen api upload
    - fixing some bugs within the repo's

---
+++ albumstore_api/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+
+//matches localhost:8888/lumen/public/
+$router->get('http://localhost/albumstore_api/public', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/artists', 'AlbumController@getAll');
+$router->get('/artist/{id}', 'AlbumController@getOne');
+$router->post('/artist/add', 'AlbumController@save');
+$router->post('/aritst/edit/{id}', 'AlbumController@update');
+$router->delete('/artist/delete/{id}', 'AlbumController@delete');
+
+$router->get('/albums', 'AlbumController@getAll');
+$router->get('/album/{id}', 'AlbumController@getOne');
+$router->post('/album/add', 'AlbumController@save');
+$router->post('/album/edit/{id}', 'AlbumController@update');
+$router->delete('/album/delete/{id}', 'AlbumController@delete');
