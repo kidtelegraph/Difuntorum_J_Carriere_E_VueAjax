26230d2a53ee08485182e645d4826b6a474d7907
kidtelegraph <jxen.dee@gmail.com>
2024-04-01 05:12:15 -0400

    db_lumen_api
    uploading files for lumen API

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Difuntorum_J_Carriere_E_VueAjax
 Vue Ajax Assignment
+
+## Album Haven
+Music and Album Gallery by Album Haven
+
+Brand Introduction:
+
+"Album Haven" offers a captivating online journey merging music and visual artistry. As an immersive album store, it invites users to explore a curated selection of album covers while seamlessly immersing themselves in the accompanying melodies. Each slide presents a captivating album cover, inviting exploration and intrigue, with the ability to instantly sample the corresponding song.
+
+Slogan: Tune in to the rhythm of life, find your haven!
+How to use
+
+This website showcases the album gallery with a music player.
+
+Running the Website Locally
+To run this website locally, follow these steps:
+
+    Clone this repository to your local machine.
+    Open the index.html using live server viewer in VS Code.
+    View it in your browser of choice
+    Browse through different album covers
+
+Enjoy!
+About the Developer
+
+Jason is a visual graphic designer responsible for minimalist, retro, and eccentric graphic design content for companies, brands, and individual clients. Ezra a front-end designer that has worked regularly on executing modern websites and graphic design projects.
+Contact Us
+
+If you have any questions or would like to explore our album store further, please feel free to contact us at albumhaven@yahoo.ca,j_difuntorum@fanshaweonline.ca and e_carriere150983@fanshaweonline.ca
+Credits
+
+This website was created using HTML, CSS, SCSS, and Javascript. Special thanks to my professors Marco De Luca, John Bennett, and Robert Haff.
---
+++ albumstore_api/album_storedb.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Mar 08, 2024 at 12:58 PM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Database: album_store
+
+-- Table structure for table artists
+
+CREATE TABLE artists (
+  id int(11) NOT NULL,
+  name varchar(255) NOT NULL,
+  genre varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table artists
+
+INSERT INTO artists (id, name, genre) VALUES
+(1, 'The Beatles', 'Pop/Rock'),
+(2, 'Michael Jackson', 'Pop'),
+(3, 'Autumn!', 'HipHop/RnB'),
+(4, 'Pierre Bourne', 'HipHop/RnB'),
+(5, 'Frank Ocean', 'RnB/Soul'),
+(6, 'Lucki', 'Cloud Rap'),
+(7, 'Bladee', 'Experimental HipHop'),
+(8, 'Prince', 'Rock/Alternative'),
+(9, 'Odd Future', 'HipHop/RnB'),
+(10, 'Pink Floyd', 'Rock/Alternative');
+
+-- Table structure for table albums
+
+CREATE TABLE albums (
+  id int(11) NOT NULL,
+  title varchar(255) NOT NULL,
+  artist_id int(11) NOT NULL,
+  release_date date NOT NULL,
+  album_image varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table albums
+
+INSERT INTO albums (id, title, artist_id, release_date, album_image) VALUES
+(1, 'Abbey Road', 1, '1969-09-26', 'abbey_road.png'),
+(2, 'Thriller', 2, '1982-11-30', 'thriller.png'),
+(3, 'Midnight Club', 3, '2023-08-22', 'midnight_club.png'),
+(4, 'The Life of Pierre Bourne', 4, '2021-05-11', 'tlop5.png'),
+(5, 'Blonde', 5, '2016-08-20', 'blonde.png'),
+(6, 'Freewave 3', 6, '2019-02-15', 'freewave3.png'),
+(7, 'Icedancer', 7, '1972-06-16', 'icedancer.png'),
+(8, 'Purple Rain', 8, '1984-06-25', 'purple_rain.png'),
+(9, 'The OF Tape Vol.2', 9, '2012-03-16', 'oftape2.png'),
+(10, 'Graduation', 10, '2007-09-11', 'graduation.png'),
+
+-- Indexes for table artists
+ALTER TABLE artists
+  ADD PRIMARY KEY (id);
+
+-- Indexes for table albums
+ALTER TABLE albums
+  ADD PRIMARY KEY (id);
+
+-- AUTO_INCREMENT for tables
+
+-- AUTO_INCREMENT for table artists
+ALTER TABLE artists
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+-- AUTO_INCREMENT for table albums
+ALTER TABLE albums
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+  
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
